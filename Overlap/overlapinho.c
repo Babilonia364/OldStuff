af12eb399d31cb6e77f50c7d0ef23560dace349b
Júnior <Babilonia364@users.noreply.github.com>
2018-12-02 00:22:11 -0200

    Update overlapinho.c

--- Overlap/overlapinho.c
+++ Overlap/overlapinho.c
@@ -1,17 +1,22 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include "grafo.h"
 
 #define MAX 9999
 #define SUB 1
 #define SUM 2
 
+double confiabilidade;
+
 void overlap(int** mat, Path *caminho_minimo1, Path *caminho_minimo2, int op, int tam)
 {
 	int i, j, k;																				/* Variaveis de laco */
 	int delete[MAX];
 	int n=0, flag=0;
+	int pConfiabilidade;
 	
 	Path *aux;
+	Vertex *vAux;
 	aux=malloc(tam*sizeof(Path));
 	
 	
@@ -52,17 +57,73 @@ void overlap(int** mat, Path *caminho_minimo1, Path *caminho_minimo2, int op, in
 			}
 		}
 	}
-	insertionSort(delete, n);
-	for(i=0, j=0; i<n; i++)
-	{
-		if(i!=delete[n])
+	
+	insertionSort(delete, n);																	/* Organizando os indices de caminhos redundantes */
+	for(i=0, j=0; i<n; i++)																		/* Caso os indices nao estejam no vetor delete */
+	{																							/* E criado outra pilha apenas com os indices a serem mantidos */
+		flag=0;
+		for(k=0; k<n; k++)																		/* Percorre o vetor de delecao */
 		{
-			aux[j]=caminho_minimo2[i];
+			if(i==delete[k])																	/* Caso o indice esteja no vetor, subir uma flag */
+			{
+				flag=1;
+				break;
+			}
+		}
+		if(flag==0)																				/* Caso a flag nao tenha sido ativada */
+		{
+			aux[j]=caminho_minimo2[i];															/* O primeiro caminho sera adicionado na nova lista */
 			j++;
 		}
 	}
 	
 	/* Passo 3 */
+	if(j==1)																					/* Caso o tamanho da nova pilha seja 1 */
+	{																							/* Neste caso j sera o tamanho da nova pilha */
+		if(op==SUM)																				/* Definindo a confiabilidade total do grafo */
+			for(i=0; i<j; i++)
+			{
+				pConfiabilidade=1;
+				for(vAux=aux->head; vAux!=NULL; vAux=vAux->prox)
+					pConfiabilidade *= vAux->prob;
+				confiabilidade += pConfiabilidade;
+			}
+		else
+			{
+				pConfiabilidade=1;
+				for(vAux=aux->head; vAux!=NULL; vAux=vAux->prox)
+					pConfiabilidade *= vAux->prob;
+				confiabilidade -= pConfiabilidade;
+			}
+	}
+	
+	/* Passo 4 */
+	if(op==SUM)																				/* Definindo a confiabilidade total do grafo */
+		for(i=0; i<j; i++)
+		{
+			pConfiabilidade=1;
+			for(vAux=aux->head; vAux!=NULL; vAux=vAux->prox)
+				pConfiabilidade *= vAux->prob;
+			confiabilidade += pConfiabilidade;
+		}
+	else
+		for(i=0; i<j; i++)
+		{
+			pConfiabilidade=1;
+			for(vAux=aux->head; vAux!=NULL; vAux=vAux->prox)
+				pConfiabilidade *= vAux->prob;
+			confiabilidade -= pConfiabilidade;
+		}
+	
+	/* Passo 5 */
+	if(j>1)
+		for(i=1; i<j; i++)
+		{
+			
+		}
+		
+	/* Passo 6 */
+	return;
 }
 
 int main()
@@ -81,7 +142,7 @@ int main()
 	printf("Confiabilidade: %lf\n", vProbabilidade);
 	
 	//Gerando caminhos minimos
-	caminho_minimo=multiPath(n, mat);
+	caminho_minimo=multiPath(n, mat, vProbabilidade);
 	
 	/* 1 vai ser sub */
 	overlap(mat, caminho_minimo, caminho_minimo, SUB, n)
