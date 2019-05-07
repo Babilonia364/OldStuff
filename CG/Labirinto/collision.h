7fd8172cd8c6a7afa7b46815eafaa24416d87434
Júnior <Babilonia364@users.noreply.github.com>
2019-05-07 19:52:02 -0300

    Create collision.h

---
+++ CG/Labirinto/collision.h
@@ -0,0 +1,3 @@
+void ParametrosIluminacao(GLfloat R, GLfloat G, GLfloat B);
+void colision(double* pos_x, double* pos_y, double* pos_x2, double* pos_y2, double* vel, double* wQad, unsigned char c);
+void checkSomething(double somex1, double somey1, double somex2, double somey2, double* ant, double* p_x, double* p_y);
